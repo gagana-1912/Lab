b38e89124b0d2eda21a5569f0118b224df803ea3
Gagana Shree <1nc24ci034@ncetmail.com>
2026-06-11 20:23:01 +0530

    Fix add function and update main execution

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-def add(a,b):
-  return a+b
-if __name__ == "__main__":
-  result = a+b
-  print(result)
+def add(a, b):
+    return a + b
+if __name__ == "__main__":  # pragma: no cover
+    result = add(2, 3)
+    print(result)
